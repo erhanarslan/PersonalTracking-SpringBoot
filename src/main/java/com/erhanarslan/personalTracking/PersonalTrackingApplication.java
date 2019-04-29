37db23e41686c32dcf585b2fc7cb425c36ba868a
Erhan Arslan <help.earslan@gmail.com>
2019-04-29 22:40:14 +0300

    initial commit

---
+++ src/main/java/com/erhanarslan/personalTracking/PersonalTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.erhanarslan.personalTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTrackingApplication.class, args);
+	}
+
+}
